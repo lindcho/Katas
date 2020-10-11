1aecf114507fb6e69124e5d7f6ec535402c20235
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2020-10-11 12:43:09 +0200

    improve test names
    add more tests

--- BowlingGameTests/BowlingGameTests.cs
+++ BowlingGameTests/BowlingGameTests.cs
@@ -7,12 +7,19 @@ namespace BowlingGameTests
     {
 
         [Test]
-        public void RollMany_ShouldReturn0()
+        public void TestBowlingGameClass()
         {
+            var sut = new Game();
+            Assert.That(sut, Is.InstanceOf<Game>());
+        }
+
+        [Test]
+        public void TestGutterGame_ShouldReturn0()
+        {
+            var sut = new Game();
             const int n = 20;
             const int pins = 0;
 
-            var sut = new Game();
             RollMany(sut, n, pins);
 
             Assert.AreEqual(0, sut.Score());
@@ -20,12 +27,23 @@ namespace BowlingGameTests
         }
 
         [Test]
-        public void RollMany_TestAllOnesReturns20()
+        public void TestAllOnes_Returns20()
         {
             var sut = new Game();
-            RollMany(sut, 20, 1);
+            const int n = 20;
+            const int pins = 1;
 
+            RollMany(sut, n, pins);
             Assert.AreEqual(20, sut.Score());
+        }
+
+        [Test]
+        public void TestAllTwos_TestAllOnesReturns40()
+        {
+            var sut = new Game();
+            RollMany(sut, 20, 2);
+
+            Assert.AreEqual(40, sut.Score());
         }
 
         [Test]
@@ -49,6 +67,51 @@ namespace BowlingGameTests
             RollMany(sut, 16, 0);
             Assert.AreEqual(24, sut.Score());
         }
+
+        [Test]
+        public void TestPerfectGame()
+        {
+            var sut = new Game();
+            RollMany(sut, 12, 10);
+            Assert.AreEqual(300, sut.Score());
+        }
+
+        [Test]
+        public void TestRandomGameNoExtraRoll()
+        {
+            var sut = new Game();
+            var randomRolls = new int[] { 1, 3, 7, 3, 10, 1, 7, 5, 2, 5, 3, 8, 2, 8, 2, 10, 9, 0 };
+            foreach (var randomRoll in randomRolls)
+            {
+                sut.Roll(randomRoll);
+            }
+            Assert.AreEqual(131, sut.Score());
+        }
+
+        [Test]
+        public void TestRandomGameWithSpareThenStrikeAtEnd()
+        {
+            var sut = new Game();
+            var randomRollsWithSpareAndStrike = new int[] { 1, 3, 7, 3, 10, 1, 7, 5, 2, 5, 3, 8, 2, 8, 2, 10, 9, 1, 10 };
+            foreach (var randomRoll in randomRollsWithSpareAndStrike)
+            {
+                sut.Roll(randomRoll);
+            }
+            Assert.AreEqual(143, sut.Score());
+        }
+
+        [Test]
+        public void TestRandomGameWithThreeStrikesAtEnd()
+        {
+            var sut = new Game();
+            var randomRollsWith3Strike = new int[] { 1, 3, 7, 3, 10, 1, 7, 5, 2, 5, 3, 8, 2, 8, 2, 10, 10, 10, 10 };
+            foreach (var randomRoll in randomRollsWith3Strike)
+            {
+                sut.Roll(randomRoll);
+            }
+            Assert.AreEqual(163, sut.Score());
+        }
+
 
         private static void RollStrike(Game sut)
         {
