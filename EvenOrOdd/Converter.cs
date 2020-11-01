725e869276b8d35ea408531efc267f47ec0dcf84
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2020-11-01 11:08:47 +0200

    add tests for even numbers

--- EvenOrOdd/Converter.cs
+++ EvenOrOdd/Converter.cs
@@ -6,15 +6,26 @@ namespace EvenOrOdd
     {
         public string PrintOddOrEvenResult(int number)
         {
+            var result = "";
+
             if (number < 0)
             {
                 throw new ArgumentNullException(nameof(number), "number cannot be negative");
             }
-            if (number % 2 != 0)
+            if (IsOddNumber(number))
             {
-                return "Odd";
+                result = "Odd";
             }
-            throw new NotImplementedException();
+
+            if (IsEvenNumber(number))
+            {
+                result = "Even";
+            }
+
+            return result;
         }
+
+        private static bool IsOddNumber(int number) => (number % 2 != 0);
+        private static bool IsEvenNumber(int number) => number >= 2 && number % 2 == 0;
     }
 }
--- EvenOrOddTests/ConverterTests.cs
+++ EvenOrOddTests/ConverterTests.cs
@@ -16,11 +16,41 @@ namespace EvenOrOddTests
             Assert.That(exception.Message, Is.EqualTo("number cannot be negative (Parameter 'number')"));
         }
 
-
         [TestCase(1, "Odd")]
         [TestCase(3, "Odd")]
         [TestCase(5, "Odd")]
-        public void Given1OddNumber_ShouldPrintOddNumber(int number, string expected)
+        public void GivenOddNumber_ShouldPrintOdd(int number, string expected)
+        {
+            var sut = new Converter();
+            var result = sut.PrintOddOrEvenResult(number);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [TestCase(1, "Odd")]
+        [TestCase(13, "Odd")]
+        [TestCase(23, "Odd")]
+        public void GivenTwoDigitOddNumber_ShouldPrintOdd(int number, string expected)
+        {
+            var sut = new Converter();
+            var result = sut.PrintOddOrEvenResult(number);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+
+        [TestCase(2, "Even")]
+        [TestCase(4, "Even")]
+        [TestCase(6, "Even")]
+        public void GivenEvenNumber_ShouldPrintEven(int number, string expected)
+        {
+            var sut = new Converter();
+            var result = sut.PrintOddOrEvenResult(number);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [TestCase(22, "Even")]
+        [TestCase(44, "Even")]
+        [TestCase(86, "Even")]
+        public void GivenTwoDigitEvenNumber_ShouldPrintEven(int number, string expected)
         {
             var sut = new Converter();
             var result = sut.PrintOddOrEvenResult(number);
