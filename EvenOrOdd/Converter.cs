92355d2ee5df15fde821eaf50066163e16d5afed
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2020-11-04 22:10:42 +0200

    wip: add ConverterTests to cater for StartAndEndRange

--- EvenOrOdd/Converter.cs
+++ EvenOrOdd/Converter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EvenOrOdd
 {
@@ -24,6 +25,25 @@ namespace EvenOrOdd
 
             return result;
         }
+
+
+        public string PrintOddEven(int startRange, int endRange)
+        {
+            var result = "";
+            for (var number = startRange < 0 ? 1 : startRange; number <= endRange; number++)
+            {
+                if (number != endRange)
+                {
+                    result += number + ",";
+
+                }
+                else
+                    result += number;
+            }
+
+            return result;
+        }
+
 
         private static bool IsOddNumber(int number) => (number % 2 != 0);
         private static bool IsEvenNumber(int number) => number >= 2 && number % 2 == 0;
