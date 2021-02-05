2cf74885875c02a36b9f0ca8d35fa514e3f76906
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2021-02-05 09:34:37 +0200

    Codewars : Add Factorial kata solution

---
+++ 7kyu_Factorial/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _7kyu_Factorial
+{
+    public class Calculator
+    {
+        public static int Factorial(int n)
+        {
+            if (n < 0 || n > 12)
+            {
+                throw new ArgumentOutOfRangeException(nameof(n));
+            }
+            if (n == 0) return 1;
+            return n * Factorial(n - 1);
+
+        }
+    }
+}
