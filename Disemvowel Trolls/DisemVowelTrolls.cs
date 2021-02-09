d39eae1f2e37de126c5b60925efdc792ceb20d1e
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2021-02-09 09:30:50 +0200

    Codewars : Add Disemvowel kata solution

---
+++ Disemvowel Trolls/DisemVowelTrolls.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace Disemvowel_Trolls
+{
+    public class DisemVowelTrolls
+    {
+        public static string Disemvowel(string str)
+        {
+            return new string(str.Where(x => "aeiou".IndexOf(x.ToString(), StringComparison.InvariantCultureIgnoreCase) < 0).ToArray());
+        }
+    }
+}
