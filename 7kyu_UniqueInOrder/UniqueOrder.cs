7dd91862620880c0b4a8ac1bae6b5eb28f2fb9bb
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2021-02-09 11:20:28 +0200

    Codewars : Add UniqueInOrder kata solution

---
+++ 7kyu_UniqueInOrder/UniqueOrder.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace _7kyu_UniqueInOrder
+{
+    public class UniqueOrder
+    {
+        public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+        {
+            var newList = new List<T>();
+            foreach (var item in iterable)
+            {
+                var newListCounter= newList.Count;
+                if (newListCounter < 1 || !(item.Equals(newList[newListCounter - 1])))
+                {
+                    newList.Add(item);
+                }
+            }
+            return newList;
+        }
+    }
+}
---
+++ 7kyu_UniqueInOrder/UniqueOrderTests.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace _7kyu_UniqueInOrder
+{
+    public class UniqueOrderTests
+    {
+        [Test]
+        public void EmptyTest()
+        {
+            Assert.AreEqual("", UniqueOrder.UniqueInOrder(""));
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual("ABCDAB", UniqueOrder.UniqueInOrder("AAAABBBCCDAABBB"));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual("ABCcAD", UniqueOrder.UniqueInOrder("ABBCcAD"));
+        }
+
+    }
+}
