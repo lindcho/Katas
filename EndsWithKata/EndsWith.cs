fe720128663cb6307cf4506320765b9c6a801ffb
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2021-03-08 08:29:30 +0200

    Codewars : Add ends with kata solution

---
+++ EndsWithKata/EndsWith.cs
@@ -0,0 +1,12 @@
+﻿namespace EndsWithKata
+{
+    public class EndsWith
+    {
+        public static bool Solution(string str, string ending)
+        {
+            // TODO: complete
+            return str.EndsWith(ending);
+
+        }
+    }
+}
---
+++ EndsWithKata/EndsWithTests.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+
+namespace EndsWithKata
+{
+    public class EndsWithTests
+    {
+        [TestFixture]
+        public class SolutionTest
+        {
+            private static object[] sampleTestCases = new object[]
+            {
+                new object[] {"samurai", "ai", true},
+                new object[] {"sumo", "omo", false},
+                new object[] {"ninja", "ja", true},
+                new object[] {"sensei", "i", true},
+                new object[] {"samurai", "ra", false},
+                new object[] {"abc", "abcd", false},
+                new object[] {"abc", "abc", true},
+                new object[] {"abcabc", "bc", true},
+                new object[] {"ails", "fails", false},
+                new object[] {"fails", "ails", true},
+                new object[] {"this", "fails", false},
+                new object[] {"abc", "", true},
+                new object[] {":-)", ":-(", false},
+                new object[] {"!@#$%^&*() :-)", ":-)", true},
+                new object[] {"abc\n", "abc", false},
+            };
+
+            [Test, TestCaseSource("sampleTestCases")]
+            public void SampleTest(string str, string ending, bool expected)
+            {
+                Assert.AreEqual(expected, EndsWith.Solution(str, ending));
+            }
+        }
+    }
+}
