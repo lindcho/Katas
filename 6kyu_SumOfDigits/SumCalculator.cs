05605d8e4409b95811fe645c1a92c9f2658eae98
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2021-03-16 16:42:54 +0200

    Codewars : Add 6kyu_SumOfDigits kata solution (DigitalRoot)

---
+++ 6kyu_SumOfDigits/SumCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _6kyu_SumOfDigits
+{
+    public class SumCalculator
+    {
+        public int DigitalRoot(long n)
+        {
+            if (n <= 9)
+            {
+                return Math.Abs((int)n);
+            }
+            long sum = 0;
+            while (n != 0)
+            {
+                sum += Math.Abs(n % 10);
+                n /= 10;
+            }
+
+            return DigitalRoot(sum);
+        }
+    }
+}
