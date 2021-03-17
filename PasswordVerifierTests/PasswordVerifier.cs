740a65c9b342a3e2700d2cf4f25999560f176ec3
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2021-03-17 09:32:29 +0200

    Wip: add PasswordVerifier kata

---
+++ PasswordVerifierTests/PasswordVerifier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PasswordVerifierTests
+{
+    public class PasswordVerifier
+    {
+        public bool Verify(string password) => IsPasswordPassedLengthRule(password, 8);
+
+        private bool IsPasswordPassedLengthRule(string password, int length)
+        {
+            if(password.Length <= length)
+                throw new Exception("Length Rule Violation. Password should be of more than characters length.");
+            return true;
+        }
+    }
+}
---
+++ PasswordVerifierTests/TestPasswordVerifier.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace PasswordVerifierTests
+{
+    [TestFixture]
+    public class TestPasswordVerifier
+    {
+        [Test]
+        public void Verify_Given4LetterPassword_ShouldThrowException()
+        {
+            //arrange
+            var sut = new PasswordVerifier();
+            //act
+            var exception = Assert.Throws<System.Exception>(() => sut.Verify("test"));
+            //assert
+            exception.Message.Should().Be("Length Rule Violation. Password should be of more than characters length.");
+        }
+    }
+}
