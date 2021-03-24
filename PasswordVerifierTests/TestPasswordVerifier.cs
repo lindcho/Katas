3637f6f786433be6dbce881c8eddf3b0f39ad420
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2021-03-24 22:47:31 +0200

    wip: work on adding rules to PasswordVerifier

--- PasswordVerifierTests/TestPasswordVerifier.cs
+++ PasswordVerifierTests/TestPasswordVerifier.cs
@@ -7,7 +7,7 @@ namespace PasswordVerifierTests
     public class TestPasswordVerifier
     {
         [Test]
-        public void Verify_Given4LetterPassword_ShouldThrowException()
+        public void Verify_GivenPasswordIsLessThan8Characters_ShouldThrowException()
         {
             //arrange
             var sut = new PasswordVerifier();
@@ -16,5 +16,39 @@ namespace PasswordVerifierTests
             //assert
             exception.Message.Should().Be("Length Rule Violation. Password should be of more than characters length.");
         }
+
+        [Test]
+        public void Verify_GivenPasswordNull_ShouldThrowException()
+        {
+            //arrange
+            var sut = new PasswordVerifier();
+            //act
+            var exception = Assert.Throws<System.Exception>(() => sut.Verify(null));
+            //assert
+            exception.Message.Should().Be("Length Rule Violation. Password should not be null.");
+        } 
+        
+        [Test]
+        public void Verify_GivenPasswordDoesNotContainAtleast1UpperCase_ShouldThrowException()
+        {
+            //arrange
+            var sut = new PasswordVerifier();
+            //act
+            var exception = Assert.Throws<System.Exception>(() => sut.Verify("abcde1234"));
+            //assert
+            exception.Message.Should().Be("Rule Violation. password should have one uppercase letter at least.");
+        }
+
+        [Test]
+        public void Verify_GivenPasswordDoesNotContainAtleastUpperCase_ShouldThrowException()
+        {
+            //arrange
+            var sut = new PasswordVerifier();
+            //act
+            var exception = Assert.Throws<System.Exception>(() => sut.Verify("Abcde1234"));
+            //assert
+            exception.Message.Should().Be("Rule Violation. password should have one uppercase letter at least.");
+        }
+
     }
 }
